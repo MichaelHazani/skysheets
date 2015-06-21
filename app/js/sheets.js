bb38a4ce62b1d0e58c0d00a0fc1737297e51cf2b
MICHAEL HAZANI <123mickey@gmail.com>
2015-06-21 15:45:46 -0700

    add rain

--- app/js/sheets.js
+++ app/js/sheets.js
@@ -1,3 +1,4 @@
+'use strict';
 
 //weather global vars
 var planeText1, planeText2, planeText3, planeText4;
@@ -7,16 +8,108 @@ var textSize = 0.3;
 var textHeight = 0.1;
 
 //threeJS init vars
-var scene, camera, renderer, cube, hemilight, plane1, plane2, plane3, plane4;
+var scene, camera, listener,
+    renderer, cube, hemilight,
+    plane1, plane2, plane3, plane4;
 var planeSpeed = 0.015;
 var gui, Timeline, controller, controller2, controller3;
 
+
+// //    ****************webAudio stuff*********************
+// //WebAudio init
+// var context;
+
+// try {
+//   // still needed for Safari
+//   window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+//   // create an AudioContext
+//   context = new AudioContext();
+// } catch(e) {
+//   // API not supported
+//   throw new Error('Web Audio API not supported.');
+// }
+
+// // Next, we’ll load a sound. The binary audio data is
+// // loaded into an ArrayBuffer via Ajax. In the onload callback, i
+// // t’s decoded using the AudioContext’s decodeAudioData method.
+// // The decoded audio is then assigned to our sound variable.
+
+// // Next, we’ll load a sound. The binary audio data is
+// // loaded into an ArrayBuffer via Ajax. In the onload callback, i
+// // t’s decoded using the AudioContext’s decodeAudioData method.
+// // The decoded audio is then assigned to our sound variable.
+
+// var sound;
+
+// /**
+//  * Example 1: Load a sound
+//  * @param {String} src Url of the sound to be loaded.
+//  */
+
+// var rain = "../audio/heavyRain.mp3"
+// var panner = context.createPanner();
+// var listener = context.listener;
+
+
+// var listenerData = document.querySelector('.listener-data');
+// var pannerData = document.querySelector('.panner-data');
+
+// function loadSound(url) {
+//   var request = new XMLHttpRequest();
+//   request.open('GET', url, true);
+//   request.responseType = 'arraybuffer';
+
+//   request.onload = function() {
+//     // request.response is encoded... so decode it now
+//     context.decodeAudioData(request.response, function(buffer) {
+//       sound = buffer;
+//       playSound(sound);
+//     }, function(err) {
+//       throw new Error(err);
+//     });
+//   }
+
+//   request.send();
+// }
+
+// // To play a sound, we need to take the AudioBuffer containing
+// // our sound, and use it to create an AudioBufferSourceNode.
+// // We then connect the AudioBufferSourceNode to the AudioContext’s
+// // destination and call the start() method to play it.
+
+// /**
+//  * Example 2: Play a sound
+//  * @param {Object} buffer AudioBuffer object - a loaded sound.
+//  */
+
+// var gainNode = context.createGain();
+
+// function playSound(buffer) {
+//   var source = context.createBufferSource();
+
+//   source.connect(gainNode);
+//   source.buffer = buffer;
+//   gainNode.connect(context.destination);
+//   source.loop = true;
+//   source.start(0);
+// }
+
+// loadSound(rain);
+
+//  ****************ThreeJS stuff*********************
+
 function init(){
+
+
 
 //standard vars
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
   camera.position.z = 10;
+  listener = new THREE.AudioListener();
+  camera.add(listener);
+
   renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
   renderer.setSize( window.innerWidth, window.innerHeight );
   $('#threeJSContainer').append( renderer.domElement );
@@ -69,14 +162,20 @@ window.addEventListener('resize', function () {
   cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
+
+//audio:
+
+  // var rain = new THREE.Audio(listener);
+  // rain.load('../audio/heavyRain.mp3');
+  // rain.setRefDistance(5);
+  //rain.autoplay = false;
+
+
+
 //setup strings:
 for (var i = 0; i < sheets.days.length; i++) {
   sheets.pt[i] = sheets.days[i] + ": " + planeTexts[i];
 }
-// pt1 = days[0] + ": " + planeTexts[0];
-// pt2 = days[1] + ": " + planeTexts[1];
-// pt3 = days[2] + ": " + planeTexts[2];
-// pt4 = days[3] + ": " + planeTexts[3];
 
 //append strings to planes:
   var text1word = new THREE.TextGeometry( sheets.pt[0], {
@@ -116,24 +215,6 @@ for (var i = 0; i < sheets.days.length; i++) {
   var text4 = new THREE.Mesh(text4word, wrapper);
 
 
-// test canvas text
-// var canvas = document.createElement('canvas');
-// var size = 250;
-// canvas.width = size;
-// canvas.height = size;
-// var context = canvas.getContext('2d');
-// context.font = '48px Courier New';
-// context.fillText('testing testing', 0, size/2);
-// var newTexture = new THREE.Texture(canvas);
-// newTexture.needsUpdate = true;
-// var sprite = new THREE.Sprite({
-//   map: newTexture,
-//   transparent: true,
-//   useScreenCoordinates: false
-// });
-// scene.add(sprite);
-
-
 //transparent planes
   var geometry1 = new THREE.BoxGeometry(20,20,2);
   var material1 = new THREE.MeshPhongMaterial({color: 0xde64de, transparent: true, wireframe: false, opacity: 0.3});
@@ -142,8 +223,10 @@ for (var i = 0; i < sheets.days.length; i++) {
   scene.add(plane1);
   text1.position.y = plane1.position.y + 10;
   text1.position.x = plane1.position.x -10;
-
   plane1.add(text1);
+
+
+
 
 
   var geometry2 = new THREE.BoxGeometry(20,20,2);
@@ -179,6 +262,7 @@ for (var i = 0; i < sheets.days.length; i++) {
 
 
 
+
   // scrollwheel to zoom
     $('#threeJSContainer').bind('mousewheel', function(e){
       e.preventDefault();
@@ -191,6 +275,7 @@ for (var i = 0; i < sheets.days.length; i++) {
   });
 
 }
+
 
 function render() {
 
